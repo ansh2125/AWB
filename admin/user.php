5fd1d7ac1cd73febdf40c92b1b43056cdb8ea02a
Ansh Shukla <125562745+ansh2125@users.noreply.github.com>
2024-09-30 11:26:20 +0530

    Update user.php

--- admin/user.php
+++ admin/user.php
@@ -3,7 +3,7 @@
   ************************************************************
   *                        AWB                                 *
   *                       Copyright                              *
-  *   Gyan Prakash Pandey   *   Ansh Shukla   *   Shiwan Tripathi   *
+  *   Gyan Prakash Pandey   *   Ansh Shukla   *   Shivam Tripathi   *
   *   Code is open source, but please do not remove this credit. *
   ************************************************************
 */?>
@@ -395,7 +395,7 @@
   ************************************************************
   *                        AWB                                 *
   *                       Copyright                              *
-  *   Gyan Prakash Pandey   *   Ansh Shukla   *   Shiwan Tripathi   *
+  *   Gyan Prakash Pandey   *   Ansh Shukla   *   Shivam Tripathi   *
   *   Code is open source, but please do not remove this credit. *
   ************************************************************
 */?>
